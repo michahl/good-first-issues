94518bb78032de9d094b44e6540b2beea180f5c7
Michail Christoforatos <mchristoforatos@icloud.com>
2024-10-15 00:31:09 +0300

    changed text color

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ import Issues from "./components/Issues"
 const App = () => {
   const [checked, setChecked] = useState(false)
   return (
-    <div className="bg-zinc-100 dark:bg-gray-950">
+    <div className="">
       <div className="mx-5 flex flex-col justify-center items-center min-h-screen">
-        <div className="w-full max-w-3xl">
+        <div className="w-full max-w-3xl flex flex-col gap-1">
           <Header />
           <Filter />
           <div className='mb-3 mt-2 flex items-center gap-2'>
-            <label htmlFor="bookmark" className="text-sm text-zinc-600 dark:text-zinc-100 opacity-90">View saved ones</label>
-            <input type="checkbox" name="bookmark" id="bookmark" onChange={() => setChecked(!checked)}/>
+            <label htmlFor="bookmark" className="text-sm text-zinc-200/80 opacity-90">View saved ones</label>
+            <input className="ring-transparent" type="checkbox" name="bookmark" id="bookmark" onChange={() => setChecked(!checked)}/>
           </div>
           <Issues checked={checked}/>
           <Footer />
